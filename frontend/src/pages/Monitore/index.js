49e8c00cde3d7c2e210250cd3f2d65a951c3b469
Matheus Castro <m.s.castro@hotmail.com>
2020-04-27 19:51:09 -0400

    graph held together

--- frontend/src/pages/Monitore/index.js
+++ frontend/src/pages/Monitore/index.js
@@ -18,40 +18,50 @@ const useStyles = makeStyles((theme) => ({
         boxShadow: 'none'
     },
     contentcard: {
-        backgroundColor: "#3656AF",
+        backgroundColor: "#434344",
         color: 'white',
-        font: '20px Roboto',
+        font: '14px Roboto',
         marginTop: '10px',
         borderRadius: 10,
-        
     },
     btn: {
         padding: theme.spacing(2),
         // maxWidth: '100%',
-        width: '100%'
     },
-    paper: {
+    commands: {
         padding: theme.spacing(2),
         textAlign: 'center',
-        paddingTop: '20px',
-        width: '47%',
-        height: '90%',
         position: 'absolute',
+    },
+    realtime: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        width: '47%',
+        height: '500px',
+        position: 'absolute',
+    },
+    graphposition: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        width: '3px',
+        height: '3px',
+        boxShadow: 'none',
+
     }
 }));
 
 const BorderLinearProgress = withStyles({
     root: {
-        transform: (props) => props.axis === 'x' ? 'rotate(135deg)': (props.axis === 'y' ? 'rotate(45deg)' : 'rotate(-90deg)'),
-        left: (props) => props.axis === 'x' ? '49%': (props.axis === 'y' ? '49%' : '50%'),
-        top: (props) => props.axis === 'x' ? '39%': (props.axis === 'y' ? '40%' : '40%'),
-        position: 'absolute',
-        margin: '10px 10px',
+        // transform: (props) => props.axis === 'x' ? 'rotate(135deg)': (props.axis === 'y' ? 'rotate(45deg)' : 'rotate(-90deg)'),
+        // left: (props) => props.axis === 'x' ? '49%': (props.axis === 'y' ? '49%' : '50%'),
+        // top: (props) => props.axis === 'x' ? '39%': (props.axis === 'y' ? '40%' : '40%'),
+        // position: 'absolute',
+        // margin: '10px 10px',
         height: 10,
-        width: 150,
+        width: 'inherit',
         backgroundColor: lighten('#3656AF', 0.9),
         borderRadius: 20,
-        transformOrigin: '0 100%',
+        // transformOrigin: '0 100%',
     },
     bar: {
         borderRadius: 20,
@@ -84,26 +94,11 @@ const Monitore = (props) => {
             <Topbar pageName="Monitoramento" />
             <div className="monitoring_mode_container">
                 <Grid container spacing={3} style={{padding: '20px'}}>
-                    <Grid item xs={12}>
+                    <Grid item xs={6}>
                         <Paper className={classes.btn}>
                             <button className="button" type="submit">Iniciar</button>
-                            
                         </Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <div className="axis-graph1">
-                                <h1>Tempo-Real</h1>
-                                <div className="axis-graph">
-                                    <BorderLinearProgress variant="determinate" value={completed} axis="x"/>
-                                    <BorderLinearProgress variant="determinate" value={completed*0.5} axis="y"/>
-                                    <BorderLinearProgress variant="determinate" value={completed*0.9} axis="z"/>
-                                </div>
-                            </div>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6} >
-                        <Paper className={classes.paper}>
+                        <Paper className={classes.commands}>
                             <div>
                                 <h1>Comandos</h1>
                                 <Card className={classes.root}>
@@ -112,6 +107,31 @@ const Monitore = (props) => {
                             </div>        
                         </Paper>
                     </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.realtime}>
+                            <div>
+                                <h1>Tempo-Real</h1>
+                                <div className="graph">
+                                    <Paper className={classes.graphposition}>
+                                        <div className="x">
+                                            <BorderLinearProgress variant="determinate" value={completed} axis="x"/>
+                                        </div>
+                                        
+                                        <div className="y">
+                                            <BorderLinearProgress variant="determinate" value={completed*0.5} axis="y"/>
+                                        </div>
+                                        
+                                        <div className="z">
+                                            <BorderLinearProgress variant="determinate" value={completed*0.9} axis="z"/>
+                                        </div>
+                                    </Paper>
+                                </div>
+                            </div>
+                        </Paper>
+                    </Grid>
+                    {/* <Grid item xs={6} >
+                        
+                    </Grid> */}
                 </Grid>
             </div>
         </Menu>
